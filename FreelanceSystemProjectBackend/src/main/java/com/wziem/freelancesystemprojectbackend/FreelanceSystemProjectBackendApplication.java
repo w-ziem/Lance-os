4430e49d1fcfd75e0a2586e46c01d1c379c45fba
w-ziem <ziembowiczwojciech@gmail.com>
2026-03-12 15:47:16 +0100

    push backend setup to repo

---
+++ FreelanceSystemProjectBackend/src/main/java/com/wziem/freelancesystemprojectbackend/FreelanceSystemProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.wziem.freelancesystemprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelanceSystemProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreelanceSystemProjectBackendApplication.class, args);
+    }
+
+}
---
+++ FreelanceSystemProjectBackend/src/test/java/com/wziem/freelancesystemprojectbackend/FreelanceSystemProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wziem.freelancesystemprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceSystemProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
