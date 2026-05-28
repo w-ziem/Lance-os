fd0da9734ceda14035e060b6967b541cd29ee003
w-ziem <ziembowiczwojciech@gmail.com>
2026-05-29 00:23:56 +0200

    feat(server): add TaskStatus and TaskPriority enums

---
+++ server/src/main/java/com/wziem/lancebackend/model/enums/TaskPriority.java
@@ -0,0 +1,5 @@
+package com.wziem.lancebackend.model.enums;
+
+public enum TaskPriority {
+    LOW, MEDIUM, HIGH
+}
---
+++ server/src/main/java/com/wziem/lancebackend/model/enums/TaskStatus.java
@@ -0,0 +1,5 @@
+package com.wziem.lancebackend.model.enums;
+
+public enum TaskStatus {
+    TODO, IN_PROGRESS, DONE
+}
